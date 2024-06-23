0df9f5a9722e08c676a64b0eda0a206aba829ce4
mohamad <201106@ppu.edu.ps>
2024-06-23 15:32:43 +0300

    manipulate various functions categories

---
+++ functions.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = var.region
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "tags" {
+  type = list
+  default = ["firstec2","secondec2"]
+}
+
+variable "ami" {
+  type = map
+  default = {
+    "us-east-1" = "ami-08a0d1e16fc3f61ea"
+    "us-west-2" = "ami-0b20a6f09484773af"
+    "ap-south-1" = "ami-0e1d06225679bc1c5"
+  }
+}
+
+
+resource "aws_instance" "app-dev" {
+   ami = lookup(var.ami,var.region)
+   instance_type = "t2.micro"
+   count = length(var.tags)
+   
+   tags = {
+     Name = element(var.tags,count.index)
+     CreationDate = formatdate("DD MMM YYYY hh:mm ZZZ",timestamp())
+   }
+}
+
+locals {
+
+time = formatdate("DD MMM YYYY hh:mm ZZZ",timestamp())
+}
+
+output "time" {
+value = local.time
+
+}
+#  In `terraform console`  if i typed the following it will go to current directory and print .gitignore content:
+#  > file("${path.module}/.gitignore") 
+# 
